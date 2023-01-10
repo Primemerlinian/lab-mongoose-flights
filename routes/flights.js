f5b64d34dacd7f17bb346b4dd0912f9e4c162aa2
Primemerlinian <mwatson0518@gmail.com>
2023-01-09 21:43:36 -0500

    display the new flight form

--- routes/flights.js
+++ routes/flights.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import * as flightsCtrl from '../controllers/flights.js'
 
 const router = Router()
 
@@ -6,7 +7,9 @@ const router = Router()
 router.get('/', function(req, res) {
   res.send('respond with a resource')
 })
+router.get('/new', flightsCtrl.new)
 
 export {
-  router
+  router,
+  newFlight as new,
 }
